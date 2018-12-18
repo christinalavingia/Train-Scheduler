d6c495d660c9be538cd5454c6b645fe5c79c434f
ChristinaLavingia <christinalavingia@gmail.com>
2018-12-17 23:01:10 -0800

    Committing updated code to reflect proper use of Firebase and data persistance.

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,84 +1,55 @@
-// Initializes Firebase
- var config = {
+var config = {
     apiKey: "AIzaSyB1Ceu217BWTDvXWFGLuNwegmEZxy6iL8I",
     authDomain: "train-scheduler-9fe5b.firebaseapp.com",
     databaseURL: "https://train-scheduler-9fe5b.firebaseio.com",
     projectId: "train-scheduler-9fe5b",
     storageBucket: "train-scheduler-9fe5b.appspot.com",
     messagingSenderId: "863352627172"
-  };
-  firebase.initializeApp(config);
+};
 
-var database = firebase.database();
+firebase.initializeApp(config);
 
- // Creates an on-click event when the submit button is clicked
- $("#add-train").on("click", function(event) {
-    event.preventDefault();
+var db = firebase.database();
+var ref = db.ref();
 
-// Stores user input as an object
-    var name = $("#train-name").val().trim();
-    var destination = $("#destination").val().trim();
-    var time = moment($("#train-time").val().trim());
-    var frequency = $("#frequency").val().trim();
-
-// Creates a temporary object for train data
-var trainData = {
-    name: name,
-    destination: destination,
-    time: time,
-    frequency: frequency 
+function addTrain(newTrainObj) {
+    ref.push(newTrainObj);
 }
-// Pushes new train data to database
-    database.ref().push(trainData);
 
-// Logs new train data to the console
-    console.log(trainData.name);
-    console.log(trainData.destination);
-    console.log(trainData.time);
-    console.log(trainData.frequency);
-
-// Clears all text boxes
-    $("#train-name").val("");
-    $("#destination").val("");
-    $("#train-time").val("");
-    $("#frequency").val("");
-});
-
-// Creates Firebase event for adding a train to the database and a row to the HTML when a user adds a train
-database.ref().on("child_added", function(childSnapshot) {
-    console.log(childSnapshot.val());
-
-// Stores database values as variables
-var trainName = childSnapshot.val().name;
-var trainDestination = childSnapshot.val().destination;
-var trainTime = childSnapshot.val().time;
-var trainFrequency = childSnapshot.val().frequency;
-
-// Logs train info to the console
-console.log(trainName);
-console.log(trainDestination);
-console.log(trainTime);
-console.log(trainFrequency);
-
-// Calculates next arrival
-var diffTime = moment().diff(moment.unix(trainTime), "minutes");
-console.log(diffTime);
-var timeRemainder = moment().diff(moment.unix(trainTime), "minutes") % trainFrequency;
-
-// Minutes before next train
-var minutesAway = trainFrequency - timeRemainder;
-
-// Next train arrival
-var nextArrival = moment().add(minutesAway, "m").format("HH:mm A");
-
-// Pushes new trains to the table
-var newRow = $("<tr>").append(
-    $("<td>").text(trainName),
-    $("<td>").text(trainDestination),
-    $("<td>").text(trainFrequency),
-    $("<td>").text(nextArrival),
-    $("<td>").text(minutesAway)
-);
+function addToTable(newTrainObj) {
+    var newRow = $("<tr>");
+    var nameCol = $("<td>").text(newTrainObj.name);
+    newRow.append(nameCol);
+    var destinationCol = $("<td>").text(newTrainObj.destination);
+    newRow.append(destinationCol)
+    var frequencyCol = $("<td>").text(newTrainObj.frequency);
+    newRow.append(frequencyCol);
+    var nextTrainCol = $("<td>").text("Compute me");
+    newRow.append(nextTrainCol);
+    var minutesAwayCol = $("<td>").text("Compute me");
+    newRow.append(minutesAwayCol);
 
     $("#train-data").append(newRow);
+}
+
+ref.on("child_added", function(snapshot) {
+    console.log(snapshot.val());
+    addToTable(snapshot.val());
+});
+
+$(document).ready(function () {
+
+    $("#add-train").on("click", function (event) {
+        event.preventDefault();
+
+        var newTrain = {
+            name: $("#train-name").val().trim(),
+            destination: $("#destination").val().trim(),
+            time: $("#train-time").val().trim(),
+            frequency: $("#frequency").val().trim()
+        };
+        console.log(newTrain);
+
+        addTrain(newTrain);
+    });
 });
